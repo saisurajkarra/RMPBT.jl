5a8b41013cf793302a30e49f099ec86936512a89
Sai suraj karra <45616515+saisurajkarra@users.noreply.github.com>
2022-12-13 19:08:58 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,23 @@
 using Documenter
 using RMPBT
-
+DocMeta.setdocmeta!(RMPBT, :DocTestSetup, :(using RMPBT); recursive=true)
 makedocs(
-    sitename = "RMPBT",
+    sitename = "RMPBT.jl",
     format = Documenter.HTML(),
-    modules = [RMPBT]
+    modules = [RMPBT],
+    authors="Sai Suraj Karra <saisurajkarra.com> and contributors",
+    repo="https://github.com//RMPBT.jl/blob/{commit}{path}#{line}",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://saisurajkarra.github.io/RMPBT.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+deploydocs(;
+    repo="github.com/saisurajkarra/RMPBT.jl",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
@@ -13,3 +26,4 @@ makedocs(
 #=deploydocs(
     repo = "<repository url>"
 )=#
+
